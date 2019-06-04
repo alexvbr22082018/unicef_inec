3ba9dde6ee84690394e5f007ffbe4aa72ad523f3
AVBR_20190128 <alex_lp7@yahoo.com>
2019-06-04 06:07:48 -0500

    "FINAL"

--- creacion_imagenes.R
+++ creacion_imagenes.R
@@ -21,3 +21,21 @@ image_append(image_scale(img, "300"), stack = TRUE) %>%
   image_write("front_imgae.png")
 
 
+list.files() %>% 
+  str_subset("sense.*logo|mit.*logo") %>% 
+  map(~image_read(.x) %>% 
+        image_resize("300x300")) %>% 
+  reduce(c) %>% 
+  image_append %>% 
+  image_write("mit_sesnse.png")
+
+c(
+"monitour_home.png",
+"monitour_guide.png") %>% 
+  map(~image_read(.x) %>% 
+        image_resize("350x400!") %>% 
+        image_write(.x))
+
+image_read("data_usa.png") %>% 
+  image_resize("400x400!") %>% 
+  image_write("data_usa.png")
